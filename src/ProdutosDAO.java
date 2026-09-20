8441b2422d1a89a6e582735676f31a89a7ec6d22
Thiago Dias Lotti Jose <thiagodlotti1993@gmail.com>
2026-09-20 15:51:53 -0300

    Implementa cadastro de produtos com mensagem de sucesso

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -22,21 +22,33 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+        prep = conn.prepareStatement(sql);
+
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+    } catch (Exception erro) {
+        JOptionPane.showMessageDialog(null,
+                "Erro ao cadastrar produto: " + erro.getMessage());
+    }
+}
         
         
         //conn = new conectaDAO().connectDB();
         
         
     }
+ 
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
-
